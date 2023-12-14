8e86f7bb42393991f8fc8ff978f25a987bcb246a
gunapalani <gunapalani19@outlook.com>
2023-12-14 12:07:42 +0530

    fix: csss

--- src/app/Components/personalinfo/personalinfo.component.ts
+++ src/app/Components/personalinfo/personalinfo.component.ts
@@ -1,30 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+ 
 @Component({
   selector: 'app-personalinfo',
   templateUrl: './personalinfo.component.html',
-  styleUrls: ['./personalinfo.component.css'],
+  styleUrls: ['./personalinfo.component.scss'],
 })
-export class PersonalinfoComponent implements OnInit {
+export class PersonalinfoComponent {
   myForm!: FormGroup;
-
+ 
   constructor(private fb: FormBuilder) {}
-
+ 
   ngOnInit() {
     this.myForm = this.fb.group({
       fullName: ['', Validators.required],
       // Define other form controls here following the same pattern
     });
   }
-
+ 
   onSubmit() {
     if (this.myForm.valid) {
-      // Implement what happens on form submission
-      console.log(this.myForm.value); // Example: Log form values
+      console.log(this.myForm.value);
     } else {
-      // Handle invalid form
       console.log('Form is invalid');
     }
   }
 }
+ 
