599ad99141e1b53f62cf848c22b132963244f071
Abirami Duraipandian <abirami@theproindia.com>
2023-12-14 20:53:59 +0530

    Feat : game Action added & game configs added

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  userId = environment.gamification.userId;
+  appId = environment.gamification.applicationId;
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  clientId: '5cbd3138-486b-4450-af1d-2318b516bdf4',
+  clientSecret: 'eIF8Q~KzFTkMRI6qCxGNqUZI5.UOO.KBlSpdxbSX',
+  gamification: {
+    userId: 'efdbb9dd-18fa-4b34-95d3-495118444bf3',
+    gameId: '646758f9bfa0de03f7d45a0c',
+    applicationId: '656191ac44696d34f553edab',
+    profileSubmissionAction: '65794ab889dfc55f3e41eace',
+    competancySubmissionAction: '65794b3089dfc5d04a420184',
+    workHistorySubmissionAction: '65794c6e89dfc502b5423cda',
+  },
+};
