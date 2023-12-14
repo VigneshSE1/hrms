da6f303dc1015b68509f33bb270b9bb06e4f49eb
Vigneshwaran <vigneshwaran@theproindia.com>
2023-12-14 15:21:08 +0530

    wizard added

--- src/app/Components/wizard/wizard.component.ts
+++ src/app/Components/wizard/wizard.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wizard',
   templateUrl: './wizard.component.html',
-  styleUrls: ['./wizard.component.scss']
+  styleUrls: ['./wizard.component.scss'],
 })
 export class WizardComponent {
-
+  currentRoute!: string;
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.currentRoute = this.router.url;
+    console.log(this.currentRoute);
+  }
 }
