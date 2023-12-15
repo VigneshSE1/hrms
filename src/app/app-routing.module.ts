8e7ae132b75c74e595bebb581e0f9c523851604c
gunapalani <gunapalani19@outlook.com>
2023-12-15 20:48:40 +0530

    fix: removed comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,8 +15,7 @@ const routes: Routes = [
       { path: 'personal-info', component: PersonalinfoComponent },
       { path: 'work-history', component: WorkhistoryComponent },
       { path: 'competency', component: CompetencyComponent },
-      // Other child routes
-      { path: '', redirectTo: 'personal-info', pathMatch: 'full' }, // Redirect to default child route
+      { path: '', redirectTo: 'personal-info', pathMatch: 'full' },
     ],
   },
   {
@@ -27,8 +26,8 @@ const routes: Routes = [
     path: 'time-sheet',
     component: TimesheetComponent,
   },
-  { path: '', redirectTo: 'information', pathMatch: 'full' }, // Redirect to default path
-  { path: '**', component: PersonalinfoComponent }, // Redirect to a 404 Not Found page for unmatched routes
+  { path: '', redirectTo: 'information', pathMatch: 'full' },
+  { path: '**', component: PersonalinfoComponent },
 ];
 
 @NgModule({
