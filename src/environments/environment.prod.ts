d498d07dd3e19d1044c55d78d009d994bd99365b
Abirami Duraipandian <abirami@theproindia.com>
2023-12-15 15:50:16 +0530

    Fix : updated build chunks max limit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   clientId: '5cbd3138-486b-4450-af1d-2318b516bdf4',
   clientSecret: 'eIF8Q~KzFTkMRI6qCxGNqUZI5.UOO.KBlSpdxbSX',
   gamification: {
-    userId: '7a7dd0dd-4e38-46a1-ad20-19fa37ef46ec',
+    userId: '1528e001-1e11-4320-b38c-3cc5fb747893',
     gameId: '646758f9bfa0de03f7d45a0c',
     applicationId: '656191ac44696d34f553edab',
     profileSubmissionAction: '65794ab889dfc55f3e41eace',
