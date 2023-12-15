3fbdc62fda9df10fd9f41197c4260c9f21d8d80a
Abirami Duraipandian <abirami@theproindia.com>
2023-12-15 12:06:45 +0530

    fix : updated userId

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   clientId: '5cbd3138-486b-4450-af1d-2318b516bdf4',
   clientSecret: 'eIF8Q~KzFTkMRI6qCxGNqUZI5.UOO.KBlSpdxbSX',
   gamification: {
-    userId: '681655c3-ab8a-4db8-9006-976863ebfa9b',
+    userId: '1528e001-1e11-4320-b38c-3cc5fb747893',
     gameId: '646758f9bfa0de03f7d45a0c',
     applicationId: '656191ac44696d34f553edab',
     profileSubmissionAction: '65794ab889dfc55f3e41eace',
